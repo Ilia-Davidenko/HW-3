b06a606020ad46b8db82b4455f4d3bccd6321f13
Davidenko Ilia <Ilia_d@outlook.com>
2022-04-19 17:29:25 +0300

    2 задача

---
+++ 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+
+double x1 = Convert.ToDouble(ReadLine());
+double y1 = Convert.ToDouble(ReadLine());
+double z1 = Convert.ToDouble(ReadLine());
+double x2 = Convert.ToDouble(ReadLine());
+double y2 = Convert.ToDouble(ReadLine());
+double z2 = Convert.ToDouble(ReadLine());
+
+double c = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
+WriteLine(c);
