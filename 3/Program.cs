87365994162c67deabc19ed757575fd351ddbde4
Davidenko Ilia <Ilia_d@outlook.com>
2022-04-19 17:52:04 +0300

    3 Задача

---
+++ 3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+WriteLine("Введите число");
+int x = int.Parse(ReadLine());
+int count = 1;
+
+while (count<=x)
+{
+    double x1 = Math.Pow(count,3);
+    count++;
+    Write($"{x1} ");
+}
