0e47c69f78568bbe435bbfdc4fab02488b83c3be
Davidenko Ilia <Ilia_d@outlook.com>
2022-04-19 17:19:27 +0300

    1 задача

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+WriteLine("Введите 5-тизначное число");
+int x = int.Parse(ReadLine());
+string poli = x.ToString();
+
+if (poli[0] == poli[4] && poli[1] == poli[3]) 
+{
+    WriteLine("Число - полиндом");
+}
+else WriteLine("Не полиндром");
